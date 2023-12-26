c0f957051e0be5adb5fb145cfa7d292381defa8f
Johan Stickman <zayatelie27@gmail.com>
2023-12-26 12:51:21 +0100

    Vidéo en fond de page d'accueil

--- lib/pages/empty_token.dart
+++ lib/pages/empty_token.dart
@@ -6,7 +6,7 @@ import 'package:denonceur/src/requests.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:video_player/video_player.dart';
 
 class EmptyTokenPage extends StatefulWidget {
   const EmptyTokenPage({super.key});
@@ -16,78 +16,119 @@ class EmptyTokenPage extends StatefulWidget {
 }
 
 class _EmptyTokenPageState extends State<EmptyTokenPage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/background.mp4')
+      ..initialize().then((_) {
+        _controller.play();
+        _controller.setLooping(true);
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('🔲', style: TextStyle(fontSize: 30)),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-          const SizedBox(height: 40),
-            const Text(
-              "Bienvenue sur Dénonceur !",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-          const SizedBox(height: 150),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ReceivePassphrasePage()),
-                );
-              },
-              child: const Text(
-                "J'ai déjà une passphrase",
-                style: TextStyle(fontSize: 20),
+      body: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _controller.value.size.width,
+                height: _controller.value.size.height,
+                child: VideoPlayer(_controller),
               ),
             ),
-          const SizedBox(height: 10),
-            const Text(
-                "Tu peux récupérer ta passphrase pour transférer tes données sur un autre appareil et les synchroniser.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15)),
-          const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () async {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const Center(
-                      child: CupertinoActivityIndicator(
-                        radius: 20,
-                      ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 150),
+                const Text(
+                  "Bienvenue sur Dénonceur !",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 150),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ReceivePassphrasePage()),
                     );
                   },
-                );
-                var newToken = await register(context);
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo[800],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "J'ai déjà une passphrase",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                    "Tu peux récupérer ta passphrase pour transférer tes données sur un autre appareil et les synchroniser.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, color: Colors.white)),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () async {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const Center(
+                          child: CupertinoActivityIndicator(
+                            radius: 20,
+                          ),
+                        );
+                      },
+                    );
+                    var newToken = await register(context);
 
-                final prefs = await SharedPreferences.getInstance();
-                if (newToken != null) {
-                  prefs.setString('token', newToken);
-                  // Afficher la page HomePage
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                }
-              },
-              child: const Text(
-                "Je n'ai pas de passphrase",
-                style: TextStyle(fontSize: 20),
-              ),
+                    final prefs = await SharedPreferences.getInstance();
+                    if (newToken != null) {
+                      prefs.setString('token', newToken);
+                      // Afficher la page HomePage
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo[800],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Je n'ai pas de passphrase",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                    "Lors de la création de ta passphrase tu n'as pas besoin de donner une information personnelle.\nPour t'identifier, une passphrase te sera attribuée.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, color: Colors.white)),
+              ],
             ),
-          const SizedBox(height: 10),
-            const Text(
-                "Lors de la création de ta passphrase tu n'as pas besoin de donner une information personnelle.\nPour t'identifier, une passphrase te sera attribuée.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15)),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
